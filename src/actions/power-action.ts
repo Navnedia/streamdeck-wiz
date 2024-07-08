51f34579589f79d3458773b3337f140c6e317685
Navnedia <87249556+Navnedia@users.noreply.github.com>
2024-07-07 20:30:30 -0500

    Updated boilerplate action

--- src/actions/power-action.ts
+++ src/actions/power-action.ts
@@ -1,17 +1,15 @@
 import { action, KeyDownEvent, SingletonAction, WillAppearEvent } from "@elgato/streamdeck";
-
 /**
- * An example action class that displays a count that increments by one each time the button is pressed.
+ * An action to toggle the power state of the selected lights on or off.
  */
-@action({ UUID: "com.navnedia.wiz-controller.increment" })
-export class IncrementCounter extends SingletonAction<CounterSettings> {
+@action({ UUID: "com.navnedia.wiz-controller.power" })
+export class PowerAction extends SingletonAction<{}> {
 	/**
 	 * The {@link SingletonAction.onWillAppear} event is useful for setting the visual representation of an action when it become visible. This could be due to the Stream Deck first
 	 * starting up, or the user navigating between pages / folders etc.. There is also an inverse of this event in the form of {@link streamDeck.client.onWillDisappear}. In this example,
 	 * we're setting the title to the "count" that is incremented in {@link IncrementCounter.onKeyDown}.
 	 */
-	onWillAppear(ev: WillAppearEvent<CounterSettings>): void | Promise<void> {
-		return ev.action.setTitle(`${ev.payload.settings.count ?? 0}`);
+	onWillAppear(ev: WillAppearEvent<{}>): void | Promise<void> {
 	}
 
 	/**
@@ -20,20 +18,7 @@ export class IncrementCounter extends SingletonAction<CounterSettings> {
 	 * and action information where applicable. In this example, our action will display a counter that increments by one each press. We track the current count on the action's persisted
 	 * settings using `setSettings` and `getSettings`.
 	 */
-	async onKeyDown(ev: KeyDownEvent<CounterSettings>): Promise<void> {
-		// Determine the current count from the settings.
-		let count = ev.payload.settings.count ?? 0;
-		count++;
-
-		// Update the current count in the action's settings, and change the title.
-		await ev.action.setSettings({ count });
-		await ev.action.setTitle(`${count}`);
+	async onKeyDown(ev: KeyDownEvent<{}>): Promise<void> {
 	}
 }
 
-/**
- * Settings for {@link IncrementCounter}.
- */
-type CounterSettings = {
-	count: number;
-};
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,12 +1,12 @@
 import streamDeck, { LogLevel } from "@elgato/streamdeck";
 
-import { IncrementCounter } from "./actions/increment-counter";
+import { PowerAction } from "./actions/power-action";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel(LogLevel.TRACE);
 
-// Register the increment action.
-streamDeck.actions.registerAction(new IncrementCounter());
+// Register the power action.
+streamDeck.actions.registerAction(new PowerAction());
 
 // Finally, connect to the Stream Deck.
 streamDeck.connect();
